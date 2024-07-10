46e6037bd6f7d0df6aa9e03293bed7c482e0e2bc
cadencarlson36 <149719551+cadencarlson36@users.noreply.github.com>
2024-07-10 14:08:16 -0700

    oops

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+            SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
